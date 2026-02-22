3a645c04e826d6c91ddcf20635f0fa91b8b923af
Xthoj <thaoxamong@gmail.com>
2026-02-22 15:38:34 -0600

    Iteration 1:
    Project setup, database, and basic structure.
    
    File contents of our project

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$dbname = "indiclex_db";
+$username = "indiclex_user";   // new user
+$password = "indiclex123";     // password you just set
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", 
+                    $username, 
+                    $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
